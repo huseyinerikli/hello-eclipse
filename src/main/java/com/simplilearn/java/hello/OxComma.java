bde26d601ddc7abdaf9d473cf0dd4a233ca82ee5
Huseyin Erikli <herikli33@gmail.com>
2023-09-25 16:01:11 -0400

    pushing 2 oxcomma classes

--- src/main/java/com/simplilearn/java/hello/OxComma.java
+++ src/main/java/com/simplilearn/java/hello/OxComma.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class OxfordComma {
+public class OxComma {
 
 	public static String printOxComma(List<String> list) {
 
 		int size = list.size();
-		if (list.isEmpty()) {			
-			return "";			
+		if (list.isEmpty()) {
+			return "";
+			// System.out.println("");
 		} else if (size == 1) {
 			return list.get(0);
 		} else if (size == 2) {
 			return list.get(0) + " and " + list.get(1);
+
 		} else {
 			StringBuilder st = new StringBuilder();
 			for (int i = 0; i < size - 1; i++) {
 				st.append(list.get(i)).append(", ");
+				// System.out.print(list.append(s).append(", "));
 			}
 			st.append("and ").append(list.get(size - 1));
 			return st.toString();
@@ -26,21 +29,18 @@ public class OxfordComma {
 	}
 
 	public static void main(String[] args) {
+
 		List<String> st = new ArrayList<String>();
-		Scanner sc = new Scanner(System.in);
 
-		for (;;) {
-			System.out.println("Enter a string (or type 'STOP' for exit and display the list): ");
-			String input = sc.nextLine();
-			if (input.equals("STOP")) {
-				System.out.print("The list is: ");
-				break;
-			}
-			st.add(input);
-		}
-		String res = OxfordComma.printOxComma(st);
-		System.out.println(res);
+		st.add("A");
+		st.add("B");
+		st.add("C");
+		st.add("D");
+		st.add("E");
 
-		sc.close();
+		System.out.println("The list is: " + OxfordComma.printOxComma(st));
+//		String res = OxfordComma.printOxComma(st);
+//		System.out.println("The list is: " + res);
 	}
+
 }
